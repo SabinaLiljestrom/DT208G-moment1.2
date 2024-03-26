771d294a3c0bbbba4daeb4df05642cd26e5574b6
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-03-26 13:58:00 +0100

    skapat grunden för webbplatsen forumlär och funktion för att skriva ut inmatad data på skärm samt inputvalidering för progression

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+// Funktion för att skriva ut kursdetaljer
+function printCourseDetails(course) {
+    var courseDetailsDiv = document.getElementById("courseDetails");
+    if (courseDetailsDiv) {
+        var newCourseDiv = document.createElement("div");
+        newCourseDiv.innerHTML = "\n            <h2>Kursinformation:</h2>\n            <p><strong>Kurskod:</strong> ".concat(course.code, "</p>\n            <p><strong>Namn:</strong> ").concat(course.name, "</p>\n            <p><strong>Progression:</strong> ").concat(course.progression, "</p>\n            <p><strong>L\u00E4nk kursplan:</strong> ").concat(course.syllabus, "</p>\n          ");
+        courseDetailsDiv.appendChild(newCourseDiv);
+    }
+}
+// Hämta DOM-element för formulär och kursdetaljer
+var courseForm = document.getElementById("courseForm");
+// Lägg till händelselyssnare på formuläret
+courseForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Hämta värden som skrivs in i formuläret
+    var codeInput = document.getElementById("code");
+    var nameInput = document.getElementById("name");
+    var progressionInput = document.getElementById("progression");
+    var syllabusInput = document.getElementById("syllabus");
+    // Här ska inputvalidering läggas till
+    if (progressionInput.value.length < 1 || progressionInput.value.length > 2) {
+        alert("Progression måste vara mellan 1 och 2 tecken långt");
+        return;
+    }
+    // Skapa ett kursobjekt och spara det i localStorage
+    var newCourse = {
+        code: codeInput.value,
+        name: nameInput.value,
+        progression: progressionInput.value,
+        syllabus: syllabusInput.value,
+    };
+    printCourseDetails(newCourse);
+});
---
+++ src/js/main.ts
@@ -0,0 +1,52 @@
+// Interface för användare
+interface CourseInfo {
+    code: string;
+    name: string;
+    progression: any;
+    syllabus: string;
+  }
+
+    // Funktion för att skriva ut kursdetaljer
+    function printCourseDetails(course: CourseInfo): void {
+        const courseDetailsDiv = document.getElementById("courseDetails");
+        if (courseDetailsDiv) {
+          const newCourseDiv = document.createElement("div");
+          newCourseDiv.innerHTML = `
+            <h2>Kursinformation:</h2>
+            <p><strong>Kurskod:</strong> ${course.code}</p>
+            <p><strong>Namn:</strong> ${course.name}</p>
+            <p><strong>Progression:</strong> ${course.progression}</p>
+            <p><strong>Länk kursplan:</strong> ${course.syllabus}</p>
+          `;
+          courseDetailsDiv.appendChild(newCourseDiv);
+        }
+}
+// Hämta DOM-element för formulär och kursdetaljer
+const courseForm = document.getElementById("courseForm") as HTMLFormElement;
+   
+// Lägg till händelselyssnare på formuläret
+courseForm.addEventListener("submit", (event) => {
+event.preventDefault();
+
+// Hämta värden som skrivs in i formuläret
+const codeInput = document.getElementById("code") as HTMLInputElement;
+const nameInput = document.getElementById("name") as HTMLInputElement;
+const progressionInput = document.getElementById("progression") as HTMLInputElement;
+const syllabusInput = document.getElementById("syllabus") as HTMLInputElement;
+
+   // Här ska inputvalidering läggas till
+   if (progressionInput.value.length < 1 || progressionInput.value.length > 2) {
+    alert("Progression måste vara mellan 1 och 2 tecken långt");
+    return;
+  }
+
+ // Skapa ett kursobjekt och spara det i localStorage
+ const newCourse: CourseInfo = {
+    code: codeInput.value,
+    name: nameInput.value,
+    progression: progressionInput.value,
+    syllabus: syllabusInput.value,
+};
+
+printCourseDetails(newCourse);
+});
